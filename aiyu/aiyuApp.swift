465c52633dc7e0c76cca75ca2083787f553d5806
Kelly Hum <kellyhum88@gmail.com>
2025-02-17 22:41:39 -0800

    add reference to managed object context for db

--- aiyu/aiyuApp.swift
+++ aiyu/aiyuApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct aiyuApp: App {
+    @StateObject private var persistenceController = PersistenceController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
